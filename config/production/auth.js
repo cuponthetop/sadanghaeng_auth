bf9b94fafcfe4cae0f69739841bbd00dbabf9e72
cuponthetop <cuponthetop@gmail.com>
2016-02-27 01:55:32 +0900

    auth config

--- config/production/auth.js
+++ config/production/auth.js
@@ -3,9 +3,9 @@
 module.exports = {
   authorizedClientIds: ['garim-service'],
 
-  accessTokenLifeTime: 60 * 60,
-  refreshTokenLifeTime: 1209600,
-  authCodeLifeTime: 30,
+  accessTokenLifeTime: 30 * 60 * 60,
+  refreshTokenLifeTime: 60 * 60 * 60,
+  authCodeLifeTime: 30 * 60 * 60,
 
   debug: false
 };
